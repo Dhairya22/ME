2f4dad9cdc6d9a5c0bc6e993fb1b0d82f205001f
Dhairya22 <dhairyashelar1@gmail.com>
2021-01-19 17:14:07 +0530

    Updated Site Content

--- contactform.php
+++ contactform.php
@@ -8,7 +8,7 @@ $Subject = "Email from my website";/*what subject you want to receive your email
 //Don't touch this please //
 $name = $_POST['name'];
 $email = $_POST['email'];
-$message = $_POST['message'];
+$comment = $_POST['comment'];
 
 
 // this is the headers//
@@ -16,7 +16,7 @@ $headers .= "Content-type: text/html;\r\n";
 $headers .= "From: $email"; 
 
 //the mail Function
-mail($to, $Subject, $message, $headers);
+mail($to, $Subject, $comment, $headers);
 //this message will show up when you hit Submit button//
 echo "Email has been sent! Thank you $name";
 ?>
